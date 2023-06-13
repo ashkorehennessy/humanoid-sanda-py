bf585458bb4245c6044598a09ddaf3bb40979696
ashkorehennessy <ashkorehennessy@gmail.com>
2023-06-13 19:11:33 +0800

    调整pid参数

--- pid.py
+++ pid.py
@@ -1,14 +1,14 @@
 class pid:
     def __init__(self):
-        self.Kp = 0
+        self.Kp = 3
         self.Kd = 0
         self.last_error = 0
-        self.output_max = 300
-        self.output_min = -300
+        self.output_max = 100
+        self.output_min = -100
 
     def update(self, input_value, setpoint):
         error = setpoint - input_value
         derivative = error - self.last_error
         self.last_error = error
         output = max(min(self.Kp * error + self.Kd * derivative, self.output_max), self.output_min)
-        return output
+        return int(output)
